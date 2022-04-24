038b07a4bc1c88b5bbff1a4886816f08b9b4d983
tuliomitico <tulioneedforspeed@gmail.com>
2022-04-24 00:57:45 -0300

    feat: add theme provider in app

--- packages/mobile/src/App.tsx
+++ packages/mobile/src/App.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components/native';
+
 import Home from './screens/Home';
+import light from './theme/light';
+import dark from './theme/dark';
 
 export default function App() {
-  return <Home />;
+  return (
+    <ThemeProvider theme={light}>
+      <Home />
+    </ThemeProvider>
+  );
 }
