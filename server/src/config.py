0894947f5b26c29042ac2eda4b530a6b679ccd85
tuliomitico <tulioneedforspeed@gmail.com>
2022-04-24 20:06:28 -0300

    fix: Add more configuration class and fix  create_app setup

--- server/src/config.py
+++ server/src/config.py
@@ -1,10 +1,18 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 class Config:
   DEBUG = False
+  DEVELOPMENT = False
   TESTING = False
+  SECRET_KEY = os.getenv('SECRET_KEY')
 
+class ProductionConfig(Config):
+  pass
 class DevelopmentConfig(Config):
   DEBUG = True
   DEVELOPMENT = True
+
+class TestingConfig(Config):
+  TESTING = True
--- server/src/server.py
+++ server/src/server.py
@@ -11,5 +11,6 @@ def create_app(config: t.Union[str,t.Dict[str,str],None] = None) -> Flask:
   app = Flask(__name__)
   if isinstance(config,dict):
     app.config.update(config)
+  app.config.from_object(config)
   app.register_blueprint(blp)
   return app
