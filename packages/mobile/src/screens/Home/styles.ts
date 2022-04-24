4496a5eed133f597a1dbe5cf4504e38ee58b839a
tuliomitico <tulioneedforspeed@gmail.com>
2022-04-24 00:58:23 -0300

    feat: add theme props in styles files

--- packages/mobile/src/screens/Home/styles.ts
+++ packages/mobile/src/screens/Home/styles.ts
@@ -2,9 +2,10 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  background-color: #000;
+  background-color: ${({ theme }) => theme.COLORS.PRIMARY};
   align-items: center;
   justify-content: center;
+  padding: 0 16px;
 `;
 
 export const HomeText = styled.Text`
