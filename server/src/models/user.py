6eed81b1c5e1c0249b37adf52ddd6d85a3285cea
tuliomitico <tulioneedforspeed@gmail.com>
2022-05-06 15:04:06 -0300

    chore: fix user model constraint, add enum dependecy

--- server/src/models/user.py
+++ server/src/models/user.py
@@ -9,7 +9,7 @@ class User(Base):
     id = sa.Column(saut.UUIDType(binary=False),default=uuid.uuid4,primary_key = True)
     name = sa.Column(sa.Text)
     password = sa.Column(sa.Text)
-    telephone = sa.Column(sa.Text)
+    telephone = sa.Column(sa.Text,unique=True)
     created_at = sa.Column(sa.DateTime(timezone=True),server_default=sa.func.now())
     updated_at = sa.Column(sa.DateTime(timezone=True),server_default=sa.func.now(),onupdate=sa.func.now())
 
