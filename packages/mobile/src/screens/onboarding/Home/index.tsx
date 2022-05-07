6a2144217a4e51ae44b4fb3a7e5bf5faf9383cfd
tuliomitico <tulioneedforspeed@gmail.com>
2022-05-07 17:58:59 -0300

    chore: add schemas dependencies, fix login screen

--- packages/mobile/src/screens/onboarding/Home/index.tsx
+++ packages/mobile/src/screens/onboarding/Home/index.tsx
@@ -1,17 +1,37 @@
-import { Icon } from '@rneui/base';
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { Button, Icon } from '@rneui/base';
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
 import Input from '../../../components/Input';
 
 import { Container, HomeText } from './styles';
 
 const Home: React.FC = () => {
+  const schema = Yup.object().shape({
+    telephone: Yup.string()
+      .required('Telefone obrigatório')
+      .min(11, 'Telefone não válido'),
+    password: Yup.string()
+      .required('Senha obrigatória')
+      .min(8, 'A senha deve ser maior que 8 dígitos'),
+  });
   const {
     control,
     handleSubmit,
     formState: { errors },
-  } = useForm({});
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: 'onBlur',
+    defaultValues: {
+      telephone: '',
+      password: '',
+    },
+  });
+  const handleLogin = async data => {
+    await signIn(data);
+  };
   return (
     <Container>
       <HomeText>Fast Entregas</HomeText>
@@ -35,7 +55,7 @@ const Home: React.FC = () => {
         secureTextEntry
         color="purple"
       />
-      <StatusBar style="auto" />
+      <Button />
     </Container>
   );
 };
