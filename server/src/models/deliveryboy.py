574b063daee5f342a0e4db847b177ab56675f00d
tuliomitico <tulioneedforspeed@gmail.com>
2022-05-11 12:44:19 -0300

    fix: fix delivery boy model constraints and migrations

--- server/src/models/deliveryboy.py
+++ server/src/models/deliveryboy.py
@@ -2,17 +2,18 @@ import sqlalchemy as sa
 import sqlalchemy.orm as orm
 import sqlalchemy_utils as saut
 
+from .user import User
 from database.db import Base, db_session
 
 class DeliveryBoy(Base):
   __tablename__ = 'delivery_boy'
-  id = sa.Column(sa.BigInteger,primary_key=True,autoincrement=True)
+  id = sa.Column(sa.Integer,primary_key=True,autoincrement=True)
   user = orm.relationship('User')
-  user_id = sa.Column(saut.UUIDType(binary=False))
+  user_id = sa.Column(saut.UUIDType(binary=False),sa.ForeignKey('user.id',ondelete='CASCADE'))
 
-  def __init__(self, user_id: str,*args, **kwargs) -> None:
+  def __init__(self,user,*args, **kwargs) -> None:
       super().__init__(*args, **kwargs)
-      self.user_id = user_id
+      self.user = user
 
   def create(self):
     db_session.add(self)
