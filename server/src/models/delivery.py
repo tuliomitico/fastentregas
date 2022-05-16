963cf3113a963dcc6143a955f57920bb3892bc4f
tuliomitico <tulioneedforspeed@gmail.com>
2022-05-15 23:23:57 -0300

    fix: add address entity as a foreign key

--- server/src/models/delivery.py
+++ server/src/models/delivery.py
@@ -2,19 +2,26 @@
 import enum
 
 import sqlalchemy as sa
+import sqlalchemy.orm as orm
 import sqlalchemy_utils as saut
 
 from database.db import Base, db_session
+from .address import Address
 
 class StatusType(enum.Enum):
   avaliable = 0
   catched = 1
   closed = 2
+
+class DeliveryType(enum.Enum):
+  a = 1
+  b = 2
 class Delivery(Base):
   __tablename__ = 'delivery'
   id = sa.Column(sa.Integer,primary_key=True)
   hour = sa.Column(sa.DateTime(timezone=True),server_default=sa.func.now())
-  # address
+  address = orm.relationship('Address')
+  address_id = sa.Column(sa.ForeignKey('address.id',ondelete='CASCADE'))
   telephone=sa.Column(sa.Text)
   name=sa.Column(sa.Text)
   shop=sa.Column(sa.Text)
--- server/src/schemas/delivery_schema.py
+++ server/src/schemas/delivery_schema.py
@@ -2,15 +2,18 @@ from marshmallow import fields
 from marshmallow_enum import EnumField
 
 from models.delivery import Delivery, StatusType
+from schemas.address_schema import AddressSchema
 from .schemas import ma
 
 class DeliverySchema(ma.SQLAlchemyAutoSchema):
   class Meta:
     model = Delivery
+    include_fk = True
 
   id = ma.auto_field(dump_only=True)
   name = ma.auto_field()
   telephone = ma.auto_field()
   hour = fields.DateTime(format="%H:%M:%S",dump_only=True)
+  address = fields.Nested(AddressSchema)
   shop = ma.auto_field()
   status = EnumField(StatusType)
