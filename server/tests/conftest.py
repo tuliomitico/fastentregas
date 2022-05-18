16de42eb2390c610a9a026ee2b631e99c9242f4d
tuliomitico <tulioneedforspeed@gmail.com>
2022-05-18 13:45:29 -0300

    fix: fix database fixture, add test context fixture

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -4,11 +4,10 @@ import jwt
 
 import pytest
 from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine, Connection
 from sqlalchemy.orm import  scoped_session, sessionmaker
 from src.server import create_app
-from database.db import Base, init_db, db_session as Session
-
-from tests.test_user.test_model import UserFactory
+from database.db import Base, db_session as Session
 
 engine = create_engine('sqlite:///database.sqlite3', echo=True)
 TestingLocalSession = scoped_session(sessionmaker(
@@ -16,18 +15,39 @@ TestingLocalSession = scoped_session(sessionmaker(
   autoflush=False,
   bind=engine
 ))
+@pytest.fixture(scope='session')
+def engine_alt():
+  return create_engine('sqlite:///database.sqlite3', echo=True)
+
+@pytest.fixture(scope='session')
+def tables(engine_alt):
+  Base.metadata.create_all(engine_alt)
+  yield
+  Base.metadata.drop_all(engine_alt)
+
+@pytest.fixture
+def db_session(engine_alt: Engine, tables):
+  connection = engine_alt.connect()
+  transaction = connection.begin()
+  Session.remove()
+  Session.configure(bind=connection)
+  session: scoped_session = Session()
+  yield session
+  session.close()
+  transaction.rollback()
+  connection.close()
+
+@pytest.fixture
+def test_request(app: Flask):
+  ctx = app.test_request_context()
+  ctx.push()
+  yield ctx
+  ctx.pop()
 
 @pytest.fixture
 def app():
   app = create_app('src.config.TestingConfig')
   yield app
-
-@pytest.fixture(scope="function") # or "module" (to teardown at a module level)
-def db_session(app: Flask):
-  Session.remove()
-  init_db(engine)
-  yield
-  Base.metadata.drop_all(bind=engine)
 
 @pytest.fixture(scope='function')
 def isolated_session(app: Flask):
--- server/tests/factories.py
+++ server/tests/factories.py
@@ -1,5 +1,7 @@
-from factory import alchemy
 import factory
+from factory import alchemy
+
+from models.deliveryboy import DeliveryBoy
 from models.user import User
 from database.db import db_session
 
@@ -11,3 +13,9 @@ class UserFactory(alchemy.SQLAlchemyModelFactory):
   name = factory.faker.Faker('name')
   telephone = factory.faker.Faker('phone_number',locale='pt_br')
   password= factory.faker.Faker('password')
+
+class DeliveryBoyFactory(alchemy.SQLAlchemyModelFactory):
+  class Meta:
+    model = DeliveryBoy
+    sqlalchemy_session = db_session
+  user = factory.SubFactory(UserFactory)
