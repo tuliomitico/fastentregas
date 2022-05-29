2260249b32f896d2f06eb8610e78bd0e6bbc8d77
tuliomitico <tulioneedforspeed@gmail.com>
2022-05-29 13:46:06 -0300

    fix: fix admin and delivey boy schema

--- server/src/schemas/admin_schema.py
+++ server/src/schemas/admin_schema.py
@@ -4,7 +4,7 @@ from schemas.user_schema import UserSchema
 from .schemas import ma
 from models.admin import Admin
 
-class DeliveryBoySchema(ma.SQLAlchemyAutoSchema):
+class AdminSchema(ma.SQLAlchemyAutoSchema):
   class Meta:
     model = Admin
     include_fk = True
--- server/src/schemas/delivery_boy_schema.py
+++ server/src/schemas/delivery_boy_schema.py
@@ -1,12 +1,13 @@
 from marshmallow_sqlalchemy import fields
-from schemas.user_schema import UserSchema
 
 from .schemas import ma
 from models.deliveryboy import DeliveryBoy
-
+from schemas.delivery_schema import DeliverySchema
+from schemas.user_schema import UserSchema
 class DeliveryBoySchema(ma.SQLAlchemyAutoSchema):
   class Meta:
     model = DeliveryBoy
     include_fk = True
 
   user = fields.Nested(UserSchema)
+  deliverys = fields.Nested(DeliverySchema,many=True)
