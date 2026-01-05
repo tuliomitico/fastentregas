cfd34a48509ffc38a47fdfe536063895d7d2fd15
tuliomitico <tulioneedforspeed@gmail.com>
2026-01-05 11:44:56 -0300

    feat(web): improve imports

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import {
-  Box,
-  CssBaseline,
-  Container,
-  Grid,
-  Typography,
-  Button,
-} from '@mui/material';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 export default async function Home() {
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-      <CssBaseline />
+      {/* <CssBaseline /> */}
       <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
         <Typography component={'h1'} variant="h1" gutterBottom>
           Fast Entregas
